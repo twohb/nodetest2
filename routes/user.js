d3a58ccd0e910d46ac6233164f3e4d62913e7671
Michael Jones <mijones@gmail.com>
2014-03-23 11:03:22 -0500

    It works

--- routes/user.js
+++ routes/user.js
@@ -10,3 +10,17 @@ exports.userlist = function(db) {
 		})
 	}
 };
+
+/*
+* POST to adduser.
+*/
+
+exports.adduser = function(db) {
+	return function(req, res) {
+		db.collection('userlist').insert(req.body, function(err, result){
+			res.send(
+				(err === null) ? {msg:''} : {msg:err}
+				);
+		});
+	}
+};
